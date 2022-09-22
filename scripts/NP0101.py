b807384d6f59b912ac3d7a1f063de422f86840fd
EF <efraisma.ef7@gmail.com>
2022-09-22 11:56:42 -0500

    Update NP0101.py

--- scripts/NP0101.py
+++ scripts/NP0101.py
@@ -8,22 +8,26 @@ def solution(A, X):
         m = (l + r) // 2
         if A[m] > X:
             r = m - 1
+        elif A[m] == X: return m
         else:
-            l = m
+            l = m + 1
     if A[l] == X:
         return l
     return -1
 
 
+
 print('[1,2,5,9,9], 5: ', solution([1,2,5,9,9], 5))
 print('[], 5: ', solution([], 5))
-print('[], 5: ', solution([], 5))
+print('[], -1: ', solution([], -1))
 print('[1], 5: ', solution([1], 5))
-print('[1], 1: ', solution([1], 1))
-print('[0], 0: ', solution([0], 0))
+print('[0,1], 1: ', solution([0,1], 1))
+print('[-1,0], 0: ', solution([-1,0], 0))
+print('[-1,0,1], 0: ', solution([-1,0,1], 0))
 print('[-1], 0: ', solution([-1], 0))
 print('[1,2], 1: ', solution([1,2], 1))
 print('[1,2], 3: ', solution([1,2], 3))
 print('[-1], -1: ', solution([-1], -1))
 print('[-2,-1], -1: ', solution([-2,-1], -1))
 print('[-2,-1], -3: ', solution([-2,-1], -3))
+print('[-2,-1], -2: ', solution([-2,-1], -2))
